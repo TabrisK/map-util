e9e4154b6b8b31973c47e26592fb49b305b9e897
TabrisK <Administrator@CLW004.deren.com.cn>
2016-12-20 18:06:15 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,62 @@
+/**
+ * Author: HELEX;
+ * Create Time: 2016-12-20 14:15
+ * Description:
+ */
+
+function startRead(ele) {
+    // obtain input element through DOM
+
+    var file = ele.files[0];
+    if(file){
+        getAsText(file);
+    }
+}
+
+function getAsText(readFile) {
+
+    var reader = new FileReader();
+
+    // Read file into memory as UTF-8
+    reader.readAsText(readFile, "UTF-8");
+
+    // Handle progress, success, and errors
+    reader.onprogress = updateProgress;
+    reader.onload = loaded;
+    reader.onerror = errorHandler;
+}
+
+function updateProgress(evt) {
+    if (evt.lengthComputable) {
+        // evt.loaded and evt.total are ProgressEvent properties
+        var loaded = (evt.loaded / evt.total);
+        if (loaded < 1) {
+            // Increase the prog bar length
+            // style.width = (loaded * 200) + "px";
+        }
+    }
+}
+
+function loaded(evt) {
+    // Obtain the read file data
+    var fileString = evt.target.result;
+    // Handle UTF-8 file dump
+    /*if(utils.regexp.isChinese(fileString)) {
+        //Chinese Characters + Name validation
+    }
+    else {
+        // run other charset test
+    }*/
+    // xhr.send(fileString)
+    var points = _.compact(fileString.split(/[ \n]/)).map(function(val){
+        var p =val.split(",");
+        return [Number(p[1]), Number(p[0])];
+    });
+    draw(points);
+}
+
+function errorHandler(evt) {
+    if(evt.target.error.name == "NotReadableError") {
+        // The file could not be read
+    }
+}
